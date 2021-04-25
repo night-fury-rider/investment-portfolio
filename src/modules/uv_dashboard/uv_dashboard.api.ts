f0fb93843c91955fadc4c4177258e895c450c83c
Yuvraj Patil <5191208+yuvi1422@users.noreply.github.com>
2021-04-25 11:43:59 +0530

    Corrected dashboard urls.

--- src/modules/uv_dashboard/uv_dashboard.api.ts
+++ src/modules/uv_dashboard/uv_dashboard.api.ts
@@ -2,11 +2,7 @@ import axios from 'axios';
 import * as dashboardData from './uv_dashboard.json';
 
 const getDashboardData = ()=> {
-  console.warn('process.env.REACT_APP_API_URL: ', process.env.REACT_APP_API_URL);
-  let dashboardUrl = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : dashboardData.config.apis.dashboard.url;
-  dashboardUrl = 'http://localhost:8888/api/getInvestments.json';
-  //dashboardUrl = 'http://localhost:8888/api/sensoredGetInvestments.json';
-  return axios.get(dashboardUrl);
+  return axios.get(dashboardData.config.apis.dashboard.urls[1]);
 }
 
 const UVDashboardApi = {

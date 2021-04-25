3d2ca3491cc4edf9dcf9005cffdc146722228452
Yuvraj Patil <5191208+yuvi1422@users.noreply.github.com>
2021-04-25 11:29:49 +0530

    Made header reponsive.

--- src/modules/uv_dashboard/uv_dashboard.api.ts
+++ src/modules/uv_dashboard/uv_dashboard.api.ts
@@ -2,7 +2,11 @@ import axios from 'axios';
 import * as dashboardData from './uv_dashboard.json';
 
 const getDashboardData = ()=> {
-  return axios.get(dashboardData.config.apis.dashboard.url);
+  console.warn('process.env.REACT_APP_API_URL: ', process.env.REACT_APP_API_URL);
+  let dashboardUrl = process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : dashboardData.config.apis.dashboard.url;
+  dashboardUrl = 'http://localhost:8888/api/getInvestments.json';
+  //dashboardUrl = 'http://localhost:8888/api/sensoredGetInvestments.json';
+  return axios.get(dashboardUrl);
 }
 
 const UVDashboardApi = {

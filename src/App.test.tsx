ef51d27e85332a131b0ae341ad33ba6f09e157a8
Yuvraj Patil <5191208+yuvi1422@users.noreply.github.com>
2021-05-09 20:13:15 +0530

    Integrated Enzyme. - Added few coverage reporters

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import UVHeader from './components/uv_header/uv_header';
+
+import * as headerJSONData from './components/uv_header/uv_header.json'
+import UVDashboard from './modules/uv_dashboard/uv_dashboard';
+import { UVRootState } from './root.reducer';
+
+jest.mock('react-redux');
+
+let shallowComponent: any;
+
+
+describe('***********************************App Component***********************************', ()=> {
+
+  beforeEach( () => {
+    shallowComponent = shallow(<App />);
+  });
+
+  test('Contains Header component', ()=> {
+    const headerComponent = shallowComponent.find(UVHeader);
+    expect(headerComponent).toHaveLength(1);
+  });
+
+  test('Contains Dashboard module', ()=> {
+    const dashboardComponent = shallowComponent.find(UVDashboard);
+    expect(dashboardComponent).toHaveLength(1);
+  });
+
 });
---
+++ src/globalMocks.ts
@@ -0,0 +1,11 @@
+import redux from 'react-redux'
+
+export const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+
+
+

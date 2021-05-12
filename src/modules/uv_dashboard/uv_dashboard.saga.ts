b248e588a520069661348db08b6c6d8f8f39e9ab
Yuvraj Patil <5191208+yuvi1422@users.noreply.github.com>
2021-05-12 11:57:00 +0530

    Resolved issue of app break when first instrument is largest.

--- src/modules/uv_dashboard/uv_dashboard.saga.ts
+++ src/modules/uv_dashboard/uv_dashboard.saga.ts
@@ -42,6 +42,7 @@ function* initDashboardSaga(): any {
       selectionIndex: 0,
       items: getProcessedBarChartData(currentCategory.items, 'current', true) as UVItem[]
     }
+    largestItemIndexes[categoryIndex] = 0; // As default, set largest item which is at index 0
     let categoryTotal = categoryData.categories[categoryIndex].items.reduce((itemAccumulator: number, currentItem: UVItem, itemIndex: number, items: UVItem[])=> {
       if(itemIndex > 0 && currentItem.value > items[itemIndex-1].value) {
         largestItemIndexes[categoryIndex] = itemIndex;

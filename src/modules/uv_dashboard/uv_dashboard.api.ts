9664b5f017763688683f25584a6199f96b4ea6cf
Yuvraj Patil <5191208+yuvi1422@users.noreply.github.com>
2021-06-01 12:34:53 +0530

    Corrected API path

--- src/modules/uv_dashboard/uv_dashboard.api.ts
+++ src/modules/uv_dashboard/uv_dashboard.api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import * as dashboardData from './uv_dashboard.json';
 
 const getDashboardData = ()=> {
-  return axios.get(dashboardData.config.apis.dashboard.urls[1]);
+  return axios.get(dashboardData.config.apis.dashboard.urls[0]);
 }
 
 const UVDashboardApi = {

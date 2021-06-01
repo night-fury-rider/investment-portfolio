9deb42df2dc6119786f5ec68bcd66f96b7a22a93
Yuvraj Patil <5191208+yuvi1422@users.noreply.github.com>
2021-06-01 10:44:36 +0530

    Updated types in accordance with new api

--- src/shared/Types.ts
+++ src/shared/Types.ts
@@ -10,10 +10,10 @@ export type UVCategory = {
 };
 
 export type UVSubItem = {
-  name: string;
-  shortName: string;
+  folio: string;
   investedValue: number;
   currentValue: number;
+  goal: string;
 }
 
 export type UVItem = {
@@ -21,21 +21,21 @@ export type UVItem = {
     [key: string]:  string | number| object | UVSubItem[] | undefined;
 
     id ?: number;
-    name: string;
-    shortName: string;
-    goal: string;
-    expenseRatio:  number;
-    exitLoad: number;
-    fundManager: string;
-    launchDate: string;
-    returns: object;
+    name ?: string;
+    shortName ?: string;
+    expenseRatio ?:  number;
+    average_expenseRatio ?: number;
+    exitLoad ?: number;
+    fundManager ?: string[];
+    launchDate ?: string;
+    returns ?: object;
     rating: number;
-    firstInvestmentDate: string;
-    tentetiveEndDate: string;
-    AUM: number;
-    fundHouse: object;
-    value: number;
-    subItems: UVSubItem[];
+    firstInvestmentDate ?: string;
+    tentetiveEndDate ?: string;
+    fundHouse ?: string;
+    fundSize ?: number;
+    subItems ?: UVSubItem[];
+    value: number; // Gets added as part of processing of data.
 };
 
 export type UVAction = {

96d064dceff2fb9503f0f00de30ed36672b13f18
night-fury-rider <5191208+night-fury-rider@users.noreply.github.com>
2025-01-19 09:17:38 +0530

    Updated static export directory name. - Updated page extensions.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   distDir: "build",
+  output: "export",
+  pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    // ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;

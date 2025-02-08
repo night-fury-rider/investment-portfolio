6a315b9f49f7a627a44a786c92563874c77f5c50
night-fury-rider <5191208+night-fury-rider@users.noreply.github.com>
2025-02-08 09:22:33 +0530

    Displayed Snackbar when we upload invalid data in investments.

--- src/pages/investments/index.page.tsx
+++ src/pages/investments/index.page.tsx
@@ -5,21 +5,42 @@ import data from "../../../public/data/data.json";
 import { ibmFont } from "app/fonts";
 import theme from "app/theme";
 import Header from "$/components/Header/Header";
-import { INVESTMENT_RECORDS } from "$/constants/strings.constants";
+import { ERRORS, INVESTMENT_RECORDS } from "$/constants/strings.constants";
 import { ICategory } from "$/dashboard/dashboard.types";
 import InvestmentRecords from "./InvestmentRecords";
+import { getParsedObject } from "$/services/UtilService";
+import LoggerService from "$/services/LoggerService";
+import { isDashboardDataValid } from "$/dashboard/DashboardService";
+import Snackbar from "$/components/Snackbar/Snackbar";
 
 export default function Page() {
   const [categories, setCategories] = useState(data.categories as ICategory[]);
+  const [openDataErrorSnackbar, setOpenDataErrorSnackbar] = useState(false);
 
   const updateData = (data: string) => {
-    const rawJSON = JSON.parse(data);
-    setCategories(rawJSON?.categories || []);
+    const rawJSON = getParsedObject(data);
+
+    if (isDashboardDataValid(rawJSON)) {
+      setCategories(rawJSON.categories);
+    } else {
+      LoggerService.error(ERRORS.data.corrupt);
+      setOpenDataErrorSnackbar(true);
+    }
+  };
+
+  const handleCloseSnackbar = () => {
+    setOpenDataErrorSnackbar(false);
   };
 
   return (
     <ThemeProvider theme={theme}>
       <div className={ibmFont.className}>
+        <Snackbar
+          message={ERRORS.data.corrupt}
+          open={openDataErrorSnackbar}
+          onClose={handleCloseSnackbar}
+          severity="error"
+        />
         <Header updateData={updateData} title={INVESTMENT_RECORDS.title} />
         <InvestmentRecords categories={categories} />
       </div>

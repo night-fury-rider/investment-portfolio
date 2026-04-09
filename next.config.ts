ac23aab3aa834014594801ee665d97ef1c1141ad
Yuvraj Patil <5191208+night-fury-rider@users.noreply.github.com>
2026-04-09 21:54:51 +0530

    Removed eslint config  from nextjs config since it's deprecated

--- next.config.ts
+++ next.config.ts
@@ -7,10 +7,6 @@ const nextConfig = {
   distDir: "build",
   output: "export",
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
-  eslint: {
-    // To Ignore ESLint errors. DEPLOYMENT BUILD SHOULD NOT HAVE THIS FLAG ENABLED.
-    // ignoreDuringBuilds: true,
-  },
   /* Comment out basePath and assetPrefix for testing build before uploading to server */
   basePath: basePathOnServer,
   assetPrefix: basePathOnServer,
